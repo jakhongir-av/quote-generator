5fdbd13c90920d3bc7a06503a464de7630357a13
Jakhongir AV <jakhongir.av@gmail.com>
2023-03-09 22:07:39 +0500

    Styling btns and adding functins to them

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
-//* Local quotes
-// function newQuote() {
-//     //* Pick a random quote from apiQuotes array
-//     const quote = localQuotes[Math.floor(Math.random() * apiQuotes.length)];
-//     console.log(quote)
-// }
+//* Get Local quotes
+function newQuote() {
+  //* Pick a random quote from apiQuotes array
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  authorText.textContent = !quote.author ? "Unknown" : quote.author;
+  quoteText.textContent = quote.text;
+  console.log(quote);
 
-
-
+  //* Check quote length to determine styling
+  if (quote.text.lenght > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+}
 
 //* Show New Quote
-function newQuote() {
-    //* Pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote)
-}
+// function newQuote() {
+//   //* Pick a random quote from apiQuotes array
+//   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+//   authorText.textContent = !quote.author ? "Unknown" : quote.author;
+//   quoteText.textContent = quote.text;
+
+//   //* Check quote length to determine styling
+//   if (quote.text.lenght > 120) {
+//     quoteText.classList.add("long-quote");
+//   } else {
+//     quoteText.classList.remove("long-quote");
+//   }
+// }
 
 //* Get quotes from API
 async function getQuotes() {
@@ -30,8 +50,18 @@ async function getQuotes() {
   }
 }
 
+//* Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//* Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
 //* On Load
-getQuotes();
+// getQuotes();
 
 //* Load local quotes
-//newQuote();
+newQuote();
